3eee528401613cd7dcacf0042f6e793a0f74904e
Sadeepa lakshan <as.lakshan03@gmail.com>
2026-09-07 12:06:26 +0530

    feat(app): update app components

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
-import Image from "next/image";
+import HeroSection from "../components/HeroSection";
+import Programmes from "../components/Programmes";
 
 export default function Home() {
   return (
-    <div className="flex-1 flex flex-col items-center justify-center w-full h-full min-h-[calc(100vh-100px)] p-6 bg-[#FEFEFE]">
-      <div className="relative w-full max-w-[500px] aspect-square animate-fade-in">
-        <Image
-          src="/logomain.jpg"
-          alt="The English Master Main Logo"
-          fill
-          className="object-contain"
-          priority
-        />
-      </div>
-    </div>
+    <>
+      <HeroSection />
+      <Programmes />
+    </>
   );
 }
